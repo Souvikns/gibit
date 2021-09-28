4e80a7c2cf616e3388d310747eba25808379356b
Restyled.io <commits@restyled.io>
2021-09-28 14:51:34 +0000

    Restyled by clang-format

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
-import { ThemeProvider } from "@primer/components";
 import '../index.css'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+import {ThemeProvider} from "@primer/components";
+
+function MyApp({Component, pageProps}) {
+  return (<ThemeProvider><Component { ...pageProps } />
+    </ThemeProvider>);
 }
 
 export default MyApp;
