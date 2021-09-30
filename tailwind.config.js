85353459590b35b6b311938b291ca23baed0239a
Restyled.io <commits@restyled.io>
2021-09-30 18:49:20 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  purge : [
-    './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}',
-    './lib/**/*.{js,ts,jsx,tsx}'
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode : false, // or 'media' or 'class'
-  theme : {
-    extend : {},
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
   },
-  variants : {
-    extend : {},
+  variants: {
+    extend: {},
   },
-  plugins : [],
+  plugins: [],
 };
