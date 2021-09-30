ae8735f2a27627d0a2f4a6e17b48dbdb786ff726
Restyled.io <commits@restyled.io>
2021-09-30 18:42:40 +0000

    Restyled by prettier

--- lib/components/org-navbar.jsx
+++ lib/components/org-navbar.jsx
@@ -1,6 +1,5 @@
 import Spacer from "./spacer";
 
-
 const Navbar = ({ org_name, org_url, avatar_url }) => {
   return (
     <div className="bg-white dark:bg-gray-800  shadow">
@@ -11,14 +10,13 @@ const Navbar = ({ org_name, org_url, avatar_url }) => {
               <img src={avatar_url} alt="avatar" className="h-12 w-12" />
             </div>
 
-            <div className="ml-6"> 
-            <h1 className="text-xl font-medium text-gray-600">{org_name}</h1>
+            <div className="ml-6">
+              <h1 className="text-xl font-medium text-gray-600">{org_name}</h1>
             </div>
           </div>
-
         </div>
       </Spacer>
-    </div> 
+    </div>
   );
 };
 
