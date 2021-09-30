18fd858922f62da98f478451ee5074f274c8a599
Souvik <souvikde.ns@gmail.com>
2021-10-01 00:10:15 +0530

    feat: added tailwind css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import "../index.css";
+import 'tailwindcss/tailwind.css'
 
 import { ThemeProvider } from "@primer/components";
 
@@ -11,11 +11,12 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.svg" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap"
           rel="stylesheet"
         ></link>
+
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
