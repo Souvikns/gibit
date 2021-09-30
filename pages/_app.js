fde4f70e9a95d6ee46bd0294dd5362456f3cb845
Restyled.io <commits@restyled.io>
2021-09-30 18:42:37 +0000

    Restyled by clang-format

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import Head from "next/head";
 import 'tailwindcss/tailwind.css'
 
-import { ThemeProvider } from "@primer/components";
+import {ThemeProvider} from "@primer/components";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
   return (
     <ThemeProvider>
       <Head>
@@ -13,12 +13,14 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap"
+  href =
+      "https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap"
           rel="stylesheet"
         ></link>
 
       </Head>
-      <Component {...pageProps} />
+      <Component {
+    ...pageProps} />
     </ThemeProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
+  purge :
+      [ './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}' ],
+  darkMode : false, // or 'media' or 'class'
+  theme : {
+    extend : {},
   },
-  variants: {
-    extend: {},
+  variants : {
+    extend : {},
   },
-  plugins: [],
+  plugins : [],
 }
