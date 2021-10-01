4960fe4f8fcaf832d838f282a3872b085d88a863
Souvik <souvikde.ns@gmail.com>
2021-10-01 20:45:53 +0530

    feat: integrated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -12,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+}
