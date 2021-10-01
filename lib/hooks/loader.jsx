05c243456323627a9541dd7d2507bb481992d16c
Souvik <souvikde.ns@gmail.com>
2021-10-01 19:39:14 +0530

    feat: created data models

--- lib/hooks/loader.jsx
+++ lib/hooks/loader.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import { fetchOrgDetails } from "../fetchers";
+import { fetchOrgDetails, fetchOrgIssues } from "../fetchers";
 
 export const useLoader = (org) => {
   const [Org, setOrg] = useState({});
   const [loading, setLoading] = useState(true);
+  const [orgIssues, setOrgIssues] = useState([]);
 
   useEffect(() => {
     fetchOrgDetails(org).then(({ response, error }) => {
