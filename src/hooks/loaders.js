83ab172757d33b421116b7ee23cdbfff2be59b93
souvik <souvikde.ns@gmail.com>
2021-10-02 23:02:20 +0530

    feat: created useIssue hook for fetching issues in org (#31)
    * feat: created useIssue hook for fetching issues in org
    
    * Restyle feat: created useIssue hook for fetching issues in org (#32)
    
    * Restyled by clang-format
    
    * Restyled by prettier
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Restyle feat: created useIssue hook for fetching issues in org (#33)
    
    * Restyled by clang-format
    
    * Restyled by prettier
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/hooks/loaders.js
+++ src/hooks/loaders.js
@@ -1,10 +1,10 @@
 import Axios from "axios";
 import { useEffect, useState } from "react";
 
-import { orgDetails } from "./util";
+import { orgDetails, queryConstructor } from "./util";
 
 const ORG_URL = "https://api.github.com/orgs/";
-const ISSUES_URL = "https://api.github.com/search/issues?q=";
+const ISSUES_URL = "https://api.github.com/search/issues";
 
 export const useOrg = (orgName) => {
   const [org, setOrg] = useState({});
@@ -27,7 +27,47 @@ export const useOrg = (orgName) => {
 
   useEffect(() => {
     loadOrgDetails();
-  }, []);
+  });
 
   return { isLoading, error, org };
+};
+
+export const useIssue = () => {
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const [issues, setIssues] = useState({});
+
+  const fetch = async ({ orgName, queryString, queryParams }, page = 1) => {
+    setLoading(true);
+    try {
+      const { data } = await Axios({
+        method: "GET",
+        url: `${ISSUES_URL}?q=${queryConstructor({
+          orgName,
+          queryParams,
+          queryString,
+        })}&page=${page}`,
+      });
+
+      setIssues({
+        total_count: data.total_count,
+        items: data.items.map((el) => ({
+          url: el.html_url,
+          repo_url: el.repository_url,
+          id: el.id,
+          number: el.number,
+          labels: el.labels,
+          title: el.title,
+          body: el.body,
+        })),
+      });
+
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  return { isLoading, error, issues, fetch };
 };
