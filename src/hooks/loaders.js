2721adb25d76b9bcea5ff52a45858ce2afcbc081
Souvik <souvikde.ns@gmail.com>
2021-10-02 15:27:25 +0530

    feat: created org details loader

---
+++ src/hooks/loaders.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import {orgDetails} from './util';
+
+const ORG_URL = "https://api.github.com/orgs/";
+const ISSUES_URL = "https://api.github.com/search/issues?q=";
+
+export const useOrg = (orgName) => {
+  const [org, setOrg] = useState({});
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState();
+
+  const loadOrgDetails = async () => {
+    try {
+      const { data } = await Axios({
+        method: "GET",
+        url: ORG_URL + orgName,
+      });
+      setOrg(orgDetails(data));
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    loadOrgDetails();
+  }, [])
+
+  return { 
+    isLoading,
+    error,
+    org
+   };
+};
