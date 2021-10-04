6bd0f8ae519ab517fc7944a0c0864135db75ca63
Souvik <souvikde.ns@gmail.com>
2021-10-04 14:05:25 +0530

    feat: ported design org navbar design from lib

--- src/pages/org.jsx
+++ src/pages/org.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import {useOrg} from '../hooks/loaders';
+import Navbar from '../components/org-navbar';
 
 const Org = ({org}) => {
+  const {isLoading, error, org: Org} = useOrg(org);
+
+  if(isLoading) {
+    return <h1>Loading...</h1>
+  }
+
+  if(error) {
+    return <h1>Sorry no org found</h1>
+  }
+
   return <div>
-    <h1>{org}</h1>
+    <Navbar org_name={Org.org_name} avatar_url={Org.avatar_url} org_url={Org.org_url}  />
   </div>
 }
 
