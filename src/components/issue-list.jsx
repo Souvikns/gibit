bcc2b9b0cc5e12098ea89d5eeaf3c352a3018000
Souvik <souvikde.ns@gmail.com>
2021-10-06 16:36:23 +0530

    feat: added design for issue list

--- src/components/issue-list.jsx
+++ src/components/issue-list.jsx
@@ -2,8 +2,31 @@
 import React, {useEffect} from "react";
 import { useIssue } from "../hooks/loaders";
 
+
+const IssueView = ({title, url, id}) => {
+  return <li key={id} className="py-5 px-4">
+    <a target="_blank" href={url} rel="noreferrer">{title}</a>
+  </li>
+}
+
+const IssueTable = ({issues}) => {
+  if(!issues) return null
+  return <div className="px-4 py-5 rounded-t sm:px-6">
+    <div className="bg-white shadow overflow-hidden sm:rounded-md">
+      <ul className="divide-y divide-gray-200">
+        {issues.map(el => <IssueView 
+        title={el.title} 
+        id={el.id}
+        url={el.url} 
+        />)}
+      </ul>
+    </div>
+  </div>
+}
+
 const IssueList = ({ org }) => {
   const { isLoading, fetch, issues } = useIssue();
+  console.log(issues);
 
   useEffect(() => {
     fetch({orgName: org});
@@ -14,7 +37,7 @@ const IssueList = ({ org }) => {
   }
 
   return <div>
-    {issues.items ? issues.items.map(el => <h1 key={el.id}>{el.title}</h1>): null}
+    {(typeof issues !=="undefined")? <IssueTable issues={issues.items} />: null}
   </div>;
 };
 
