bf6bae9683da3d2d4d27e0881f64b798bb00cb7e
Souvik <souvikde.ns@gmail.com>
2021-10-06 23:51:47 +0530

    feat: updated the design

--- src/components/issue-list.jsx
+++ src/components/issue-list.jsx
@@ -2,12 +2,17 @@
 import React, { useEffect } from "react";
 import { useIssue } from "../hooks/loaders";
 
-const IssueView = ({ title, url, id }) => {
+const IssueView = ({ title, url, id, body }) => {
   return (
     <li key={id} className="py-5 px-4">
-      <a target="_blank" href={url} rel="noreferrer">
-        {title}
-      </a>
+      <div className="flex flex-col">
+        <a target="_blank" href={url} rel="noreferrer" className="text-xl hover:text-green-400 font-bold">
+          {title}
+        </a>
+        <div>
+          {(body)? <h1 className="text-gray-600 w-4/5 h-20 overflow-ellipsis overflow-hidden py-4">{body}</h1>: null}
+        </div>
+      </div>
     </li>
   );
 };
@@ -19,7 +24,12 @@ const IssueTable = ({ issues }) => {
       <div className="bg-white shadow overflow-hidden sm:rounded-md">
         <ul className="divide-y divide-gray-200">
           {issues.map((el) => (
-            <IssueView title={el.title} id={el.id} url={el.url} />
+            <IssueView
+              title={el.title}
+              id={el.id}
+              url={el.url}
+              body={el.body}
+            />
           ))}
         </ul>
       </div>
