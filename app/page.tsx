4e2de7f35878eec49a9ac5ea16e8c7b9e47a4c19
souvik <souvikde.ns@gmail.com>
2024-09-13 13:26:17 +0530

    feat: add routing for org page (#44)
    * feat: add route for org page
    
    and hooked search bar in main page.
    
    * chore: formatting
    
    * Restyled by whitespace (#45)
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    ---------
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+'use client'
+import { useState } from "react"
 import Searchbar from "../components/searchbar"
+import { useRouter } from 'next/navigation'
 export default function Page() {
+    const router = useRouter()
+    const [search, setSearch] = useState('')
+
     return <div className="container mx-auto">
         <div className="flex items-center justify-center h-screen">
             <div className="flex flex-row w-2/4 justify-center space-x-3">
-                <Searchbar />
-                <button className="btn btn-primary btn-outline">Search</button>
+                <Searchbar data={search} update={setSearch} />
+                <button className="btn btn-primary btn-outline" onClick={() => {
+                    router.push(`/${search}`)
+                }}>Search</button>
             </div>
         </div>
     </div>
--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -1,7 +1,10 @@
 
-export default function Searchbar() {
+export default function Searchbar({ data, update }: { data: string, update: Function }) {
+
     return <input
         type="text"
+        value={data}
+        onChange={e => update(e.currentTarget.value)}
         placeholder="Search GitHub Organization"
         className="input input-bordered rounded-sm w-full max-w-xs"
     />
